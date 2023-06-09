56bf28b92b80e8981be5bb805314ea6f8de87726
James Gallagher <jamesg@jamesg.blog>
2023-06-09 10:13:53 +0100

    add grounding dino code, update setup file

---
+++ autodistill_grounding_dino/__init__.py
@@ -0,0 +1,3 @@
+from autodistill_grounding_dino.grounding_dino_model import GroundingDINO
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 import re
 
-with open("./src/__init__.py", 'r') as f:
+with open("./autodistill_grounding_dino/__init__.py", 'r') as f:
     content = f.read()
     # from https://www.py4u.net/discuss/139845
     version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content).group(1)
@@ -11,16 +11,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="template-python-zuppif#1",  
+    name="autodistill-grounding-dino",
     version=version,
-    author="zuppif",
-    author_email="francesco.zuppichini@gmail.com",
-    description="<INSERT_DESCRIPTION>",
+    author="Roboflow",
+    author_email="support@roboflow.com",
+    description="GroundingDINO module for use with Autodistill",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    url="https://github.com/autodistill/autodistill-grounding-dino",
     install_requires=[
-      # list your requires
+        "torch",
+        "autodistill",
+        "numpy>=1.20.0",
+        "opencv-python>=4.6.0",
+        "rf_groundingdino",
+        "rf_segment_anything",
+        "supervision"
     ],
     packages=find_packages(exclude=("tests",)),
     extras_require={
